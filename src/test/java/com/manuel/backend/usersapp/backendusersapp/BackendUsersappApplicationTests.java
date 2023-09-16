2d69e42fdc45f04c8695c0fb028ef85fb5ed5998
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-09-15 22:30:23 -0300

    First commit

---
+++ src/test/java/com/manuel/backend/usersapp/backendusersapp/BackendUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.backend.usersapp.backendusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
